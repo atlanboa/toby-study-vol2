5d8c32e8251107c9543d3076e2fea645109c8f8b
sangheon kim <atlanboa@gmail.com>
2021-02-17 16:51:11 +0900

    first commit

---
+++ src/main/java/com/toby/vol2/ConsolePrinter.java
@@ -0,0 +1,8 @@
+package com.toby.vol2;
+
+public class ConsolePrinter implements Printer{
+    @Override
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/com/toby/vol2/Hello.java
@@ -0,0 +1,23 @@
+package com.toby.vol2;
+
+public class Hello {
+    Printer printer;
+    String name;
+
+    public String sayHello() {
+        return "Hello " + name;
+    }
+
+    public void print() {
+        this.printer.print(sayHello());
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrinter(Printer printer) {
+        this.printer = printer;
+    }
+
+}
